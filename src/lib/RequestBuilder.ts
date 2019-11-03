92f24f688652e76558ca91d1cde3ed12e5870c4d
Kent Walters <kentwaltersmail@gmail.com>
2019-11-02 19:45:41 -0600

    Add 'body' to request.

--- src/lib/RequestBuilder.ts
+++ src/lib/RequestBuilder.ts
@@ -5,16 +5,45 @@ export default class RequestBuilder {
     config: AxiosRequestConfig = {};
 
     constructor(scheme: string, baseUrl: string) {
+        this.setUpBaseUrl(scheme, baseUrl)
+    }
+
+    request(method: Method, path: string, args: any, cb: any, request: any) {
+        this.setMethod(method);
+        this.addPathToUrl(path);
+        this.addParametersToRequest(request, args);
+        this.sendRequest(cb);
+    }
+
+    setUpBaseUrl(scheme: string, baseUrl: string) {
         this.config.baseURL = scheme.toLocaleLowerCase() + '://' + baseUrl;
     }
 
-    addParametersToPath(value: any, param: string){
+    sendRequest(callback: any) {
+        axios(this.config)
+            .then((response: any) => {
+                callback(RequestBuilder.convertServerResponseToResponseState(response), this.config)
+            })
+            .catch((error: any) => {
+                callback(RequestBuilder.convertServerResponseToResponseState(error.response), this.config)
+            });
+    }
+
+    setMethod(method: Method) {
+        this.config.method = method;
+    }
+
+    addPathToUrl(path: any) {
+        this.config.baseURL += path;
+    }
+
+    addParameterFromPath(value: any, param: string){
         if (this.config.baseURL) {
             this.config.baseURL = this.config.baseURL.replace(`{${param}}`, value);
         }
     }
 
-    addParametersToHeader(name: string, value: string) {
+    addParameterFromHeader(name: string, value: string) {
         if (!this.config.headers) {
             this.config.headers = {}
         }
@@ -22,58 +51,65 @@ export default class RequestBuilder {
         this.config.headers[name] = value;
     }
 
-    addParametersToQuery(params: any) {
+    addParametersFromQuery(params: any) {
         this.config.baseURL += '?';
-        this.config.baseURL += Object.keys(params).map((k) => `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`).join('&');
+        this.config.baseURL += this.encodeObject(params);
     }
 
-    addParametersToForm(params: any) {
+    addParametersFromForm(params: any) {
         this.config.baseURL += '?';
-        this.config.baseURL += Object.keys(params).map((k) => `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`).join('&');
+        this.config.baseURL += this.encodeObject(params);
     }
 
-    request(method: Method, path: string, args: any, cb: any, request: any) {
-        this.config.method = method;
+    addParameterFromBody(params: any, args: any) {
+        try {
+            this.config.data = JSON.parse(args[params.name]);
+        } catch (error) {
+            this.config.data = {};
+        }
+    }
 
-        this.config.baseURL += path;
+    encodeObject(parameters: any) {
+        return Object.keys(parameters).map((k) => `${encodeURIComponent(k)}=${encodeURIComponent(parameters[k])}`).join('&');
+    }
 
-        if (request.parameters.length) {
+    addParametersToRequest(request: any, args: any) {
+        if(request.parameters.length) {
             let queryParams: any = {};
             let formParams: any = {};
 
             request.parameters.forEach((param: any) => {
                 switch (param.in) {
                     case 'path':
-                        this.addParametersToPath(args[param.name], param.name);
+                        this.addParameterFromPath(args[param.name], param.name);
                         break;
                     case 'header':
-                        this.addParametersToHeader(param.name, args[param.name]);
+                        this.addParameterFromHeader(param.name, args[param.name]);
                         break;
                     case 'query':
-                        queryParams[param.name] = args[param.name];
+                        if (args[param.name]) {
+                            queryParams[param.name] = args[param.name];
+                        }
                         break;
                     case 'formData':
-                        queryParams[param.name] = args[param.name];
+                        if (args[param.name]) {
+                            queryParams[param.name] = args[param.name];
+                        }
+                        break;
+                    case 'body':
+                        this.addParameterFromBody(param, args);
                         break;
                 }
             });
 
             if (Object.keys(formParams).length) {
-                this.addParametersToForm(formParams);
+                this.addParametersFromForm(formParams);
             }
 
             if (Object.keys(queryParams).length) {
-                this.addParametersToQuery(queryParams);
+                this.addParametersFromQuery(queryParams);
             }
         }
-
-        axios(this.config)
-            .then((response: any) => {
-                cb(RequestBuilder.convertServerResponseToResponseState(response), this.config)
-            })
-            .catch((error: any) => {
-                cb(RequestBuilder.convertServerResponseToResponseState(error.response), this.config)
-            });
     }
 
     static convertServerResponseToResponseState(response: any): RequestResponseState {
